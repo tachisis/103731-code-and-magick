763a2c99829265c3a0e310c7d00421fb3f695825
tachisis <engel.t@gmail.com>
2017-08-14 18:26:05 +0300

    Optimize code

--- js/stat.js
+++ js/stat.js
@@ -1,40 +1,64 @@
 'use strict';
 
-window.renderStatistics = function (ctx, names, times) {
+var textParams = {
+  style: '#000',
+  font: '16px PT Mono',
+  baseline: 'top'
+};
 
-  function drawCloud(context, x, y, w, h, radius) {
-    var r = x + w;
-    var b = y + h;
-    context.beginPath();
-    context.moveTo(x + radius, y);
-    context.lineTo(r - radius, y);
-    context.quadraticCurveTo(r, y, r, y + radius);
-    context.lineTo(r, y + h - radius);
-    context.quadraticCurveTo(r, b, r - radius, b);
-    context.lineTo(r - radius, b + radius);
-    context.lineTo(r - radius * 2, b);
-    context.lineTo(x + radius, b);
-    context.quadraticCurveTo(x, b, x, b - radius);
-    context.lineTo(x, y + radius);
-    context.quadraticCurveTo(x, y, x + radius, y);
-    context.fill();
-    context.closePath();
+var cloudParams = {
+  x: 100,
+  y: 10,
+  width: 420,
+  height: 270,
+  radius: 20,
+  shadowDelta: 10,
+  cloudFill: '#fff',
+  shadowFill: 'rgba(0, 0, 0, 0.7)'
+};
+
+var histogramParams = {
+  height: 150,
+  barWidth: 40,
+  indent: 50,
+  nameMargin: 5,
+  timeMargin: -20,
+  initialX: 120,
+  initialY: 95
+};
+
+function printText(ctx, text, x, y, params) {
+  ctx.fillStyle = params.style;
+  ctx.font = params.font;
+  ctx.textBaseline = params.baseline;
+  ctx.fillText(text, x, y);
+}
+
+function drawCloud(context, x, y, width, height, radius, fill, shadow) {
+  if (shadow) {
+    x += shadow;
+    y += shadow;
   }
+  var right = x + width;
+  var bottom = y + height;
+  context.fillStyle = fill;
+  context.beginPath();
+  context.moveTo(x + radius, y);
+  context.lineTo(right - radius, y);
+  context.quadraticCurveTo(right, y, right, y + radius);
+  context.lineTo(right, y + height - radius);
+  context.quadraticCurveTo(right, bottom, right - radius, bottom);
+  context.lineTo(right - radius, bottom + radius);
+  context.lineTo(right - radius * 2, bottom);
+  context.lineTo(x + radius, bottom);
+  context.quadraticCurveTo(x, bottom, x, bottom - radius);
+  context.lineTo(x, y + radius);
+  context.quadraticCurveTo(x, y, x + radius, y);
+  context.fill();
+  context.closePath();
+}
 
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  drawCloud(ctx, 110, 20, 420, 265, 20);
-
-  ctx.fillStyle = 'rgb(255, 255, 255)';
-  drawCloud(ctx, 100, 10, 420, 265, 20);
-
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
-
-  ctx.fillText('Ура, вы победили!', 120, 40);
-  ctx.fillText('Список результатов:', 120, 60);
-
-  ctx.textBaseline = 'top';
-
+function getMaxTime(times) {
   var max = -1;
 
   for (var i = 0; i < times.length; i++) {
@@ -44,31 +68,38 @@ window.renderStatistics = function (ctx, names, times) {
     }
   }
 
-  var histogramHeight = 150;
-  var step = histogramHeight / max;
-  var barWidth = 40;
-  var indent = 50;
-  var nameMargin = 5;
-  var timeMargin = -20;
-  var initialX = 120;
-  var initialY = 90;
+  return max;
+}
 
-  for (i = 0; i < times.length; i++) {
+function getOpacity(min, max) {
+  return (Math.random() * (max - min) + min).toFixed(1);
+}
+
+function drawHistBar(ctx, barX, barY, barHeight, name, time) {
+  ctx.fillStyle = name === 'Вы' ? 'rgba(255, 0, 0, 1)' : 'rgba(0, 0, 255, ' + getOpacity(0.5, 1) + ')';
+
+  ctx.fillRect(barX, barY, histogramParams.barWidth, barHeight);
+
+  printText(ctx, name, barX, barY + barHeight + histogramParams.nameMargin, textParams);
+  printText(ctx, parseInt(time, 10), barX, barY + histogramParams.timeMargin, textParams);
+}
+
+window.renderStatistics = function (ctx, names, times) {
+
+  drawCloud(ctx, cloudParams.x, cloudParams.y, cloudParams.width, cloudParams.height, cloudParams.radius, cloudParams.shadowFill, cloudParams.shadowDelta);
+  drawCloud(ctx, cloudParams.x, cloudParams.y, cloudParams.width, cloudParams.height, cloudParams.radius, cloudParams.cloudFill);
+
+  printText(ctx, 'Ура, вы победили!', cloudParams.width / 2, 20, textParams);
+  printText(ctx, 'Список результатов:', cloudParams.width / 2, 40, textParams);
+
+  var step = histogramParams.height / getMaxTime(times);
+
+  for (var i = 0; i < times.length; i++) {
     var barHeight = times[i] * step;
-    var topIndent = histogramHeight - barHeight;
-    var barX = initialX + (indent + barWidth) * i;
-    var barY = initialY + topIndent;
+    var topIndent = histogramParams.height - barHeight;
+    var barX = histogramParams.initialX + (histogramParams.indent + histogramParams.barWidth) * i;
+    var barY = histogramParams.initialY + topIndent;
 
-    if (names[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = 'rgba(0, 0, 255, ' + (Math.random() + 0.05) + ')';
-    }
-
-    ctx.fillRect(barX, barY, barWidth, barHeight);
-
-    ctx.fillStyle = '#000';
-    ctx.fillText(names[i], barX, barY + barHeight + nameMargin);
-    ctx.fillText(parseInt(times[i], 10), barX, barY + timeMargin);
+    drawHistBar(ctx, barX, barY, barHeight, names[i], times[i]);
   }
 };
